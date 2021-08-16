5d44acb8617e3144239dbdeb488a0930b1965d7e
yushuai <yushuai@shimo.im>
2021-08-16 23:15:36 +0800

    add io/reflection

---
+++ io/src/test/java/com/javanorth/JavanorthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanorth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavanorthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
