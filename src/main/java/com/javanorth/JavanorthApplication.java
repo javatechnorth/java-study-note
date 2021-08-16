bd67bbe7250d13a97aa121bb93d475a47fb9cfc4
yushuai <yushuai@shimo.im>
2021-08-16 23:00:41 +0800

    first commit

---
+++ src/main/java/com/javanorth/JavanorthApplication.java
@@ -0,0 +1,13 @@
+package com.javanorth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavanorthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavanorthApplication.class, args);
+    }
+
+}
